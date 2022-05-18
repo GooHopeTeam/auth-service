d3455a5ddb6c05ed656ed9b489a34cdf193faba5
ignatyevm <polyndromdev@gmail.com>
2022-05-19 07:40:35 +1000

    move non-handler functions to utils.go

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,50 +1,11 @@
 package handler
 
 import (
-	"errors"
-	"log"
-	"net/http"
-	"reflect"
-
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"github.com/goohopeteam/auth-service/internal/payload"
 	"github.com/goohopeteam/auth-service/internal/service/auth"
+	"net/http"
 )
-
-func sendErrorResponse(ctx *gin.Context, err error) {
-	if errResponse, ok := err.(payload.ErrorResponse); ok {
-		ctx.JSON(http.StatusBadRequest, errResponse)
-	} else {
-		log.Printf("Enexpected error: %+v\n", err)
-		ctx.JSON(http.StatusBadRequest, payload.NewUndefinedError())
-	}
-}
-
-func extractPayload[Payload any](ctx *gin.Context) (*Payload, error) {
-	details := map[string]string{
-		"required": "required",
-		"email":    "incorrect_email",
-		"min":      "too_short",
-		"max":      "too_long",
-	}
-	var payloadBody Payload
-	payloadErr := payload.NewPayloadValidationError()
-	if err := ctx.ShouldBindJSON(&payloadBody); err != nil {
-		var vErrors validator.ValidationErrors
-		if errors.As(err, &vErrors) {
-			for _, vError := range vErrors {
-				field, _ := reflect.TypeOf(payloadBody).FieldByName(vError.StructField())
-				fieldName := field.Tag.Get("json")
-				payloadErr.Details[fieldName] = details[vError.Tag()]
-			}
-		} else {
-			return nil, err
-		}
-		return nil, payloadErr
-	}
-	return &payloadBody, nil
-}
 
 type Handler struct {
 	authService auth.AuthService
@@ -57,12 +18,12 @@ func New(authService auth.AuthService) Handler {
 func (handler Handler) HandleRegistration(ctx *gin.Context) {
 	pl, err := extractPayload[payload.RegistrationRequest](ctx)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	err = handler.authService.RegisterUser(pl)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	ctx.JSON(http.StatusOK, payload.EmptyResponse{})
@@ -72,12 +33,12 @@ func (handler Handler) HandleLogin(ctx *gin.Context) {
 	var token *payload.TokenResponse
 	pl, err := extractPayload[payload.LoginRequest](ctx)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	token, err = handler.authService.LoginUser(pl)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	ctx.JSON(http.StatusOK, token)
@@ -86,12 +47,12 @@ func (handler Handler) HandleLogin(ctx *gin.Context) {
 func (handler Handler) HandleTokenVerification(ctx *gin.Context) {
 	pl, err := extractPayload[payload.TokenVerificationRequest](ctx)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	err = handler.authService.VerifyToken(pl)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	ctx.JSON(http.StatusOK, payload.EmptyResponse{})
@@ -100,12 +61,13 @@ func (handler Handler) HandleTokenVerification(ctx *gin.Context) {
 func (handler Handler) HandleEmailVerification(ctx *gin.Context) {
 	pl, err := extractPayload[payload.EmailVerificationRequest](ctx)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
 		return
 	}
 	token, err := handler.authService.VerifyEmail(pl)
 	if err != nil {
-		sendErrorResponse(ctx, err)
+		respondWithError(ctx, err, http.StatusBadRequest)
+		return
 	}
 	ctx.JSON(http.StatusOK, token)
 }
---
+++ internal/handler/utils.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/goohopeteam/auth-service/internal/payload"
+	"log"
+	"net/http"
+	"reflect"
+)
+
+func respondWithError(ctx *gin.Context, err error, code int) {
+	if errResponse, ok := err.(payload.ErrorResponse); ok {
+		ctx.JSON(code, errResponse)
+	} else {
+		log.Printf("Enexpected error: %+v\n", err)
+		ctx.JSON(http.StatusInternalServerError, payload.NewUndefinedError())
+	}
+}
+
+func extractPayload[Payload any](ctx *gin.Context) (*Payload, error) {
+	details := map[string]string{
+		"required": "required",
+		"email":    "incorrect_email",
+		"min":      "too_short",
+		"max":      "too_long",
+	}
+	var payloadBody Payload
+	payloadErr := payload.NewPayloadValidationError()
+	if err := ctx.ShouldBindJSON(&payloadBody); err != nil {
+		var vErrors validator.ValidationErrors
+		if errors.As(err, &vErrors) {
+			for _, vError := range vErrors {
+				field, _ := reflect.TypeOf(payloadBody).FieldByName(vError.StructField())
+				fieldName := field.Tag.Get("json")
+				payloadErr.Details[fieldName] = details[vError.Tag()]
+			}
+		} else {
+			return nil, err
+		}
+		return nil, payloadErr
+	}
+	return &payloadBody, nil
+}
